8be003295e811616b5ddac522609c0c0b1bb47ec
Markus Kolb <github@tower-net.de>
2018-06-30 21:02:40 +0200

    - same for js

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,2 @@
-import { Logger } from './logger';
 export { ILoggerConfig, ILogLevel } from './i_logger_config';
-export default Logger;
+export { Logger } from './logger';
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var logger_1 = require("./logger");
-exports.default = logger_1.Logger;
+exports.Logger = logger_1.Logger;
 //# sourceMappingURL=index.js.map
